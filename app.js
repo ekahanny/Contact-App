8c4f04da4ab8f195020f970eeac753cf162a9abe
ekahanny <ekahnny@gmail.com>
2024-04-04 01:57:53 +0800

    completed the function

--- app.js
+++ app.js
@@ -1,26 +1,54 @@
-// app.js
+// Mengimpor function yang akan digunakan dari file contacts.js
+const {
+  addContacts,
+  saveContacts,
+  deleteContact,
+  searchContact,
+  bubbleSort,
+  sortContacts,
+  listContacts,
+} = require("./contacts.js");
 
-const { addContacts, saveContacts, deleteContact } = require("./contacts.js");
-
+// Function utama untuk menampilkan menu
 const main = async () => {
   const choice = await addContacts(
-    "Pilih aksi (1: Tambah Kontak, 2: Hapus Kontak) : "
+    "Pilih aksi (1: Tambah Kontak, 2: Lihat Semua Kontak, 3: Cari Kontak, 4: Urutkan Kontak, 5: Hapus Kontak) : "
   );
 
-  if (choice === "1") {
-    const nama = await addContacts("Masukkan Nama Anda : ");
-    const noHP = await addContacts("Masukkan No. HP Anda : ");
-    const alamat = await addContacts("Masukkan Alamat Anda : ");
-    saveContacts(nama, noHP, alamat);
-  } else if (choice === "2") {
-    const deleteChoice = await addContacts(
-      "Masukkan Nama atau No. HP Kontak yang ingin dihapus: "
-    );
-    await deleteContact(deleteChoice);
-  } else {
-    console.log("Pilihan tidak valid.");
-    rl.close();
+  // Menjalankan menu sesuai dengan pilihan user
+  switch (choice) {
+    case "1":
+      const nama = await addContacts("Masukkan Nama Anda : ");
+      const noHP = await addContacts("Masukkan No. HP Anda : ");
+      const alamat = await addContacts("Masukkan Alamat Anda : ");
+      saveContacts(nama, noHP, alamat);
+      break;
+    case "2":
+      listContacts();
+      break;
+    case "3":
+      const searchKey = await addContacts(
+        "Masukkan Nama atau No. HP Kontak yang ingin dicari: "
+      );
+      searchContact(searchKey);
+      break;
+    case "4":
+      sortContacts(bubbleSort);
+      listContacts();
+      break;
+    case "5":
+      const deleteChoice = await addContacts(
+        "Masukkan Nama atau No. HP Kontak yang ingin dihapus: "
+      );
+      await deleteContact(deleteChoice);
+      listContacts();
+      break;
+    default:
+      console.log("Pilihan tidak valid.");
+      rl.close();
+      break;
   }
 };
 
+// Memanggil function utama
 main();
--- contacts.js
+++ contacts.js
@@ -1,5 +1,4 @@
-// contacts.js
-
+// Menginisiasi Module
 const fs = require("fs");
 const readline = require("readline");
 
@@ -8,17 +7,21 @@ const rl = readline.createInterface({
   output: process.stdout,
 });
 
+// Mendeklarasikan path directory dari file tempat data disimpan
 const directory = "./data";
 const dataPath = "./data/contacts.json";
 
+// Menambahkan folder data (jika belum ada)
 if (!fs.existsSync(directory)) {
   fs.mkdirSync(directory);
 }
 
+// Menambahkan file contacts.json (jika belum ada)
 if (!fs.existsSync(dataPath)) {
   fs.writeFileSync(dataPath, "[]", "utf-8");
 }
 
+// Mengambil data yang dibutuhkan dari inputan user
 const addContacts = (contact) => {
   return new Promise((resolve, reject) => {
     rl.question(contact, (data) => {
@@ -27,9 +30,10 @@ const addContacts = (contact) => {
   });
 };
 
+// Menyimpan data dari hasil inputan user kedalam file contacts.json
 const saveContacts = (nama, noHP, alamat) => {
   const contact = { nama, noHP, alamat };
-  const file = fs.readFileSync(dataPath, "utf8");
+  const file = fs.readFileSync(dataPath, "utf-8");
   const contacts = JSON.parse(file);
   contacts.push(contact);
   fs.writeFileSync(dataPath, JSON.stringify(contacts));
@@ -37,8 +41,9 @@ const saveContacts = (nama, noHP, alamat) => {
   rl.close();
 };
 
+// Function untuk menghapus kontak berdasarkan nama/no hp
 const deleteContact = async (searchKey) => {
-  const file = fs.readFileSync(dataPath, "utf8");
+  const file = fs.readFileSync(dataPath, "utf-8");
   let contacts = JSON.parse(file);
 
   const index = contacts.findIndex(
@@ -58,4 +63,84 @@ const deleteContact = async (searchKey) => {
   rl.close();
 };
 
-module.exports = { addContacts, saveContacts, deleteContact };
+// Mencari kontak berdasarkan nama/no hp
+const searchContact = (searchKey) => {
+  const file = fs.readFileSync(dataPath, "utf-8");
+  const contacts = JSON.parse(file);
+
+  const foundContacts = contacts.filter(
+    (contact) =>
+      contact.nama.toLowerCase().includes(searchKey.toLowerCase()) ||
+      contact.noHP.includes(searchKey)
+  );
+
+  if (foundContacts.length > 0) {
+    console.log("Kontak yang ditemukan:");
+    foundContacts.forEach((contact) => {
+      console.log(
+        `Nama: ${contact.nama}, No. HP: ${contact.noHP}, Alamat: ${contact.alamat}`
+      );
+    });
+  } else {
+    console.log(`Kontak dengan nama atau nomor ${searchKey} tidak ditemukan.`);
+  }
+
+  rl.close();
+};
+
+// Mengurutkan kontak berdasarkan huruf awal dari nama kontak
+const bubbleSort = (arr) => {
+  const n = arr.length;
+  for (let i = 0; i < n - 1; i++) {
+    for (let j = 0; j < n - i - 1; j++) {
+      if (arr[j].nama.toLowerCase() > arr[j + 1].nama.toLowerCase()) {
+        const temp = arr[j];
+        arr[j] = arr[j + 1];
+        arr[j + 1] = temp;
+      }
+    }
+  }
+  return arr;
+};
+
+const sortContacts = () => {
+  const file = fs.readFileSync(dataPath, "utf-8");
+  let contacts = JSON.parse(file);
+
+  contacts = bubbleSort(contacts);
+  fs.writeFileSync(dataPath, JSON.stringify(contacts));
+  console.log("Kontak telah diurutkan.");
+  rl.close();
+};
+
+// Menampilkan keseluruhan daftar kontak
+const listContacts = () => {
+  const file = fs.readFileSync(dataPath, "utf-8");
+  const contacts = JSON.parse(file);
+
+  if (contacts.length > 0) {
+    console.log("Daftar Kontak:");
+    contacts.forEach((contact, index) => {
+      console.log(
+        `${index + 1}. Nama: ${contact.nama}, No. HP: ${
+          contact.noHP
+        }, Alamat: ${contact.alamat}`
+      );
+    });
+  } else {
+    console.log("Tidak ada kontak yang ditemukan.");
+  }
+
+  rl.close();
+};
+
+// Mengekspor modul agar function dapat digunakan pada file App.js
+module.exports = {
+  addContacts,
+  saveContacts,
+  deleteContact,
+  searchContact,
+  bubbleSort,
+  sortContacts,
+  listContacts,
+};
